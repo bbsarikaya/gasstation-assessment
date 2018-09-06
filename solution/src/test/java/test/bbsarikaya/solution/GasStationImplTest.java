0aa00d4a2b680ef5b8f40dd2bc18083ca12c35ab
Burcu Sarikaya <burcu.sarikaya@ozu.edu.tr>
2018-09-06 21:18:50 +0300

    Add unit tests for buyGas method

--- solution/src/test/java/test/bbsarikaya/solution/GasStationImplTest.java
+++ solution/src/test/java/test/bbsarikaya/solution/GasStationImplTest.java
@@ -9,6 +9,8 @@ import org.junit.Test;
 import me.bbsarikaya.solution.GasStationImpl;
 import net.bigpoint.assessment.gasstation.GasPump;
 import net.bigpoint.assessment.gasstation.GasType;
+import net.bigpoint.assessment.gasstation.exceptions.GasTooExpensiveException;
+import net.bigpoint.assessment.gasstation.exceptions.NotEnoughGasException;
 
 public class GasStationImplTest {
 
@@ -100,5 +102,36 @@ public class GasStationImplTest {
 
 		station.setPrice(GasType.DIESEL, -15.0);
 		assertEquals(0.0, station.getPrice(GasType.DIESEL), 0);
+	}
+	
+	@Test
+	public void testBuyGas() throws NotEnoughGasException, GasTooExpensiveException {
+		GasStationImpl station = createStation();
+	    station.buyGas(GasType.DIESEL, 10.0, 5.0);
+	}
+	
+	@Test(expected = NotEnoughGasException.class)
+	public void testBuyGasWhenNotEnoughGas() throws NotEnoughGasException, GasTooExpensiveException {
+		GasStationImpl station = createStation();
+		station.buyGas(GasType.DIESEL, 20.0, 5.0);
+	}
+	
+	public void testBuyGasWhenNoGas() throws NotEnoughGasException, GasTooExpensiveException {
+		GasStationImpl station = createStation();
+		station.buyGas(GasType.SUPER, 10.0, 5.0);
+	}
+	
+	@Test(expected = GasTooExpensiveException.class)
+	public void testBuyGasWhenGasTooExpensive() throws NotEnoughGasException, GasTooExpensiveException {
+		GasStationImpl station = createStation();
+		station.buyGas(GasType.DIESEL, 5.0, 2.0);
+	}
+
+	private GasStationImpl createStation() {
+		GasStationImpl station = new GasStationImpl();
+		station.addGasPump(new GasPump(GasType.DIESEL, 10.0));
+		station.addGasPump(new GasPump(GasType.DIESEL, 15.0));
+		station.setPrice(GasType.DIESEL, 3.0);		
+		return station;
 	}
 }
